e6a7a8a6a8466afb1592bb43c0380d519c8a54d3
RobertoNesta <robertonesta1993@gmail.com>
2023-03-08 15:25:08 +0100

    break up the ex into smaller steps

---
+++ assets/main.js/main.js
@@ -0,0 +1,14 @@
+//Richieste esercizio
+
+//chiedere numero km da compiere.
+//chiedere età del passeggero.
+// prezzo del biglietto = €0.21/km
+//sconto per minorenni 20%
+//sconto per over 65 40%
+//prezzo finale da arrotondare fino ai centesimi di euro.
+
+//Passaggi
+
+//usare prompt per chiedere numero km e età del passeggero
+//if else sull'età in modo da scontare o meno il prezzo del biglietto
+//arrotondamento del prezzo con math.round
